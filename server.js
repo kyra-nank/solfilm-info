4abd50377dcb8c0607030306389715429715ea88
Kyra <kyra.nankivell@mail.utoronto.ca>
2022-03-06 18:18:41 -0500

    fix: heroku production deployment

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ app.use(bodyParser.json());
 
 require('./routes/emailRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up specific production assets like main.js, or main.css
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file if route not recognized
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('Server running on port 5000');
